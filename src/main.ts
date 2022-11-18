6b24b82447c5c3b4955474557961c6681a04a337
Mohammad Shahi <104696903+moshahi@users.noreply.github.com>
2022-11-18 18:42:46 +0330

    add cors policy

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ origin: '*' });
   app.use(cookieParser());
   await app.listen(5000);
 }
