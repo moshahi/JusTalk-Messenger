e75f344ae7326bd2e5d25520d9763c4c231eadbe
Mohammad Shahi <104696903+moshahi@users.noreply.github.com>
2022-11-18 21:00:30 +0330

    refactor cors policy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   app.use(cookieParser());
   await app.listen(5000);
 }
